08a81c94b526de0a6c376942de23d175477e3d14
Rocio <rocardon88@gmail.com>
2022-11-25 11:59:04 +0100

    a falta de añadir los servicios

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+// import { SesionService } from '../services/sesion.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  nombre: string | null = '';
+  apellido: string | null = '';
+  constructor()
+  // (private Sesion: SesionService)
+  {
+    // this.getData();
   }
 
+  ngOnInit(): void {}
 }
+
+  // getData() {
+  //   this.nombre = localStorage.getItem('Nombre');
+  //   this.apellido = localStorage.getItem('Apellido');
+  //   this.email = localStorage.getItem('Email')
+  // }
+
