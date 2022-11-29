1e226163612dcd9ac3c03ba30ff21bc52aeb4278
Rocio <rocardon88@gmail.com>
2022-11-29 13:33:25 +0100

    carpeta services terminada

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  baseURL = 'https://api.arasaac.org/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPictogramsByCategory(categoria: string): Observable<any> {
+    return this.http.get(this.baseURL + 'pictograms/es/search/' + categoria);
+  }
+  getPictogramById(id: string): Observable<any> {
+    return this.http.get(this.baseURL + 'pictograms/es/' + id);
+  }
 }
